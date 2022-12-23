702a15f63bb3c065bbf00bd56b880bf68db06d2e
hode69 <43639379+hode69@users.noreply.github.com>
2022-12-23 07:22:39 +0700

    18th
    Done

--- My project/Assets/Script/GyroScope.cs
+++ My project/Assets/Script/GyroScope.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class GyroScope : MonoBehaviour
 {
+    public GameObject GyroError, mulai;
+
     // Start is called before the first frame update
     private void Start()
     {
         if (SystemInfo.supportsGyroscope)
         {
             Input.gyro.enabled = true;
+            mulai.SetActive(true);
         }
          else
         {
+            GyroError.SetActive(true);
             Debug.Log("Gyro is not supported on your Device");
         }
     }
